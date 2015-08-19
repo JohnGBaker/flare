06bda7f68e70c26b7b70db5c9fac18bcfc571d96
Sylvain Marsat <sylvain.marsat@ligo.org>
2015-08-19 16:00:37 -0400

    Corrected missing \n after the first line in the seed files in LISAinference.c.

--- LISAinference/LISAinference.c
+++ LISAinference/LISAinference.c
@@ -488,12 +488,12 @@ int main(int argc, char *argv[])
       fprintf(fphyslivepoints, "    %.18E", injectedparams->beta);
       fprintf(fphyslivepoints, "    %.18E", injectedparams->polarization);
       fprintf(fphyslivepoints, "   %.18E", logZtrue);
-      fprintf(fphyslivepoints, "   %d", 1); /* We impose that the injection belongs to mode no. 1 */
+      fprintf(fphyslivepoints, "   %d\n", 1); /* We impose that the injection belongs to mode no. 1 */
       /* Live points - convert to values in the cube */
       double* cubevalues = malloc(ndim*sizeof(double));
       getcubeparams(cubevalues, ndim, injectedparams, freeparamsmap);
       for(int i=0; i<ndim; i++) fprintf(flivepoints, "    %.18E", cubevalues[i]);
-      fprintf(flivepoints, "   %.18E", logZtrue); 
+      fprintf(flivepoints, "   %.18E\n", logZtrue); 
       free(cubevalues);
 
       /* Also set the resume option to true */
