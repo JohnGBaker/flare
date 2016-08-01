d0ebe319831da5fcd8eb384c673ed689a0d6fd94
John Baker <john.g.baker@nasa.gov>
2016-08-01 18:41:05 -0400

    Added Fisher matrix computation with LISAinference_ptmcmc.  This has so-far been tested to see that it gives plausible results which are mostly consistent with variations in methodology.
    The routine applies second order differencing with the differencing step set to be some small fraction (set by --Fisher_err_target) some a scalefactor (for each param).  The scalefactor starts with a generic scale which the ptmcmc prior provides, and then then the Fisher is recalculated with the scale based on the minus 1/2 power of the Fisher diagonal elements.  This is iterated until the result converges to the expected target level.
    
    The code is not particularly fast.  The slowest part is getting the Distance derivs right (which we could do analytically, of course) with the case of using non-flatprior distance scaling.

--- LISAinference/LISAinference_ptmcmc.cc
+++ LISAinference/LISAinference_ptmcmc.cc
@@ -17,7 +17,7 @@
 #include "proposal_distribution.hh"
 #include "ptmcmc.hh"
 #include "LISAinference_common.h"
-
+#include "gsl/gsl_linalg.h"
 using namespace std;
 
 typedef initializer_list<double> dlist;
@@ -28,8 +28,9 @@ shared_ptr<Random> globalRNG;//used for some debugging...
 //Global Control parameters (see more in main())
 const int Npar=9;
 int output_precision;
+double fisher_err_target=0.001;
 
-//First we define the likelihood function object
+//First we define the all-in-one likelihood function object
 class flare_likelihood : public bayes_likelihood {
   sampleable_probability_function * prior;
   int count,nevery;
@@ -39,6 +40,7 @@ class flare_likelihood : public bayes_likelihood {
   void *context;
 public:
   flare_likelihood(stateSpace *sp, void *context, sampleable_probability_function *prior=nullptr):context(context),prior(prior),bayes_likelihood(sp,nullptr,nullptr){
+    setPrior(prior);
     best=state(sp,sp->size());
     reset();
     nevery=0;
@@ -55,33 +57,16 @@ public:
   state transformDataState(const state &s)const{return s;};
   state transformSignalState(const state &s)const{return s;};
   double evaluate_log(state &s){
-    valarray<double>params=s.get_params();
     double tstart=omp_get_wtime();
-    
-    //Here the param indices are hard coded, but we can use sp to find them by name
-    LISAParams templateparams;
-    templateparams.m1 = params[0];
-    templateparams.m2 = params[1];
-    templateparams.tRef = params[2];
-    templateparams.distance = params[3];
-    if(!priorParams->flat_distprior)//If not using flat prior on distance then we need to transform from s(D) to D.
-      templateparams.distance= pow(params[3] * pow(priorParams->dist_max, 3) + (1.0 - params[3]) * pow(priorParams->dist_min, 3), 1.0 / 3.0);
-    templateparams.phiRef = params[4];
-    templateparams.inclination = params[5];
-    templateparams.lambda = params[6];
-    templateparams.beta = params[7];
-    templateparams.polarization = params[8];
-    templateparams.nbmode = globalparams->nbmodetemp; /* Using the global parameter for the number of modes in templates */
-    
+    LISAParams templateparams =state2LISAParams(s);
     double result=0;
+
     /* Note: context points to a LISAContext structure containing a LISASignal* */
     if(globalparams->tagint==0) {
-      //LISAInjectionCAmpPhase* injection = ((LISAInjectionCAmpPhase*) context);
       LISAInjectionCAmpPhase injection = *((LISAInjectionCAmpPhase*) context);
       double like;
       like = CalculateLogLCAmpPhase(&templateparams, &injection);
       result = like - logZdata;
-      //result = CalculateLogLCAmpPhase(&templateparams, injection) - logZdata;
     }
     else if(globalparams->tagint==1) {
       LISAInjectionReIm* injection = ((LISAInjectionReIm*) context);
@@ -115,21 +100,165 @@ public:
     }
     return result;
   };
+  
+  LISAParams state2LISAParams(const state &s){
+    valarray<double>params=s.get_params();
+
+    //Here the param indices are hard coded, but we can use sp to find them by name
+    LISAParams templateparams;
+    templateparams.m1 = params[0];
+    templateparams.m2 = params[1];
+    templateparams.tRef = params[2];
+    templateparams.distance = params[3];
+    if(!priorParams->flat_distprior)//If not using flat prior on distance then we need to transform from s(D) to D.
+      templateparams.distance= pow(params[3] * pow(priorParams->dist_max, 3) + (1.0 - params[3]) * pow(priorParams->dist_min, 3), 1.0 / 3.0);
+    templateparams.phiRef = params[4];
+    templateparams.inclination = params[5];
+    templateparams.lambda = params[6];
+    templateparams.beta = params[7];
+    templateparams.polarization = params[8];
+    templateparams.nbmode = globalparams->nbmodetemp; /* Using the global parameter for the number of modes in templates */
+    
+    return templateparams;
+  };
 
   ///from stateSpaceInterface
   virtual void defWorkingStateSpace(const stateSpace &sp){};
 
-  virtual void write(ostream &out,state &st){cout<<"flare_likelihood::write: No write routine defined!"<<endl;};
-  virtual void writeFine(ostream &out,state &st,int ns=-1, double ts=0, double te=0){cout<<"flare_likelihood::writeFine: No write routine defined!"<<endl;};
-  virtual void getFineGrid(int & nfine, double &tstart, double &tend)const{cout<<"flare_likelihood::getFineGrid: No routine defined!"<<endl;};
+  //virtual void write(ostream &out,state &st){cout<<"flare_likelihood::write: No write routine defined!"<<endl;};
+  //virtual void writeFine(ostream &out,state &st,int ns=-1, double ts=0, double te=0){cout<<"flare_likelihood::writeFine: No write routine defined!"<<endl;};
+  //virtual void getFineGrid(int & nfine, double &tstart, double &tend)const{cout<<"flare_likelihood::getFineGrid: No routine defined!"<<endl;};
   void print_info(){cout<<" mean = "<<total_eval_time/count<<" through "<<count<<" total evals"<<endl; };
+  double getFisher(const state &s0, vector<vector<double> >&fisher_matrix)override{
+    //First we must set the injection context
+    /* Initialize the data structure for the injection */
+    //LISAInjectionCAmpPhase* injectedsignalCAmpPhase = NULL;
+    LISAInjectionReIm* injectedsignalReIm = NULL;
+    if(globalparams->tagint==0) {
+      //LISAInjectionCAmpPhase_Init(&injectedsignalCAmpPhase);
+      //LISAGenerateInjectionCAmpPhase(injectedparams, injectedsignalCAmpPhase);
+      cout<<"Fisher matrix computation not yet implemented for AmpPhase polar wf representation (try --tagint==1)"<<endl;
+      exit(-1);
+    }
+    else if(globalparams->tagint==1) {
+      LISAInjectionReIm_Init(&injectedsignalReIm);
+      LISAGenerateInjectionReIm(injectedparams, globalparams->minf, globalparams->nbptsoverlap, 1, injectedsignalReIm); /* Use here logarithmic sampling as a default */
+    }
+    int dim=s0.size();
+    int maxFisherIter=3*dim;
+    //double deltafactor=0.001;
+    double deltafactor=fisher_err_target;
+    double tol=5000*deltafactor*deltafactor*deltafactor;
+    valarray<double> scales;
+    nativePrior->getScales(scales);
+    double err=1;
+    int count=0;
+    vector< vector<double> >last_fisher_matrix(dim,vector<double>(dim,0));
+    while(err>tol&&count<maxFisherIter){
+      //cout<<"\ncount="<<count<<"\nscales = ";for(int i=0;i<dim;i++)cout<<scales[i]<<"\t";cout<<endl;
+      for(int i=0;i<dim;i++){
+	double hi=scales[i]*deltafactor;
+	state sPlusi=s0;
+	sPlusi.set_param(i,s0.get_param(i)+hi);
+	state sMinusi=s0;
+	sMinusi.set_param(i,s0.get_param(i)-hi);
+	for(int j=i;j<dim;j++){
+	  //compute j derivative of model
+	  double hj=scales[j]*deltafactor;
+	  state sPlusj=s0;
+	  sPlusj.set_param(j,s0.get_param(j)+hj);
+	  state sMinusj=s0;
+	  sMinusj.set_param(j,s0.get_param(j)-hj);
+	  //for(int k=0;k<N;k++)dmudj[k]=(modelPlus[k]-modelMinus[k])/h/2.0;
+	  //Compute fisher matrix element
+	  //cout<<"Fisher i,j="<<i<<","<<j<<endl;
+	  //cout<<"            hi,hj="<<hi<<","<<hj<<endl;
+	  fisher_matrix[i][j]
+	    = CalculateOverlapReIm(state2LISAParams( sPlusi), state2LISAParams( sPlusj), injectedsignalReIm)
+	    - CalculateOverlapReIm(state2LISAParams( sPlusi), state2LISAParams(sMinusj), injectedsignalReIm)
+	    - CalculateOverlapReIm(state2LISAParams(sMinusi), state2LISAParams( sPlusj), injectedsignalReIm)
+	    + CalculateOverlapReIm(state2LISAParams(sMinusi), state2LISAParams(sMinusj), injectedsignalReIm);	 
+	  fisher_matrix[i][j]/=4*hi*hj;
+	  
+	  fisher_matrix[j][i] = fisher_matrix[i][j];
+	}
+      }
+      
+      //estimate error
+      err=0;
+      double square=0;
+      for(int i=0;i<dim;i++)for(int j=0;j<dim;j++){
+	  double delta=(fisher_matrix[i][j]-last_fisher_matrix[i][j]);///scales[i]/scales[j];
+	  square+=fisher_matrix[i][j]*fisher_matrix[i][j];
+	  err+=delta*delta;
+	  //if(isnan(delta)||delta*delta>tol/10)cout<<"delta["<<i<<","<<j<<"]="<<delta*delta<<endl;
+	}
+      if(isnan(err)){
+	for(int i=0;i<dim;i++)scales[i]/=3;
+	err=1;
+      }else{
+	//set scale estimate based on result
+	for(int i=0;i<dim;i++)scales[i]=1.0/sqrt(1/scales[i]+fisher_matrix[i][i]);
+	//prep for next version of fisher calc;
+	for(int i=0;i<dim;i++)for(int j=0;j<dim;j++)last_fisher_matrix[i][j]=fisher_matrix[i][j];
+      }
+      count++;
+      //cout<<"err->"<<err<<"( goal="<<tol<<")"<<endl;
+    }
+    err=sqrt(err);
+    //cout<<"err="<<err<<endl;
+    //cout<<"tol="<<tol<<endl;
+    if(err<tol)return tol;
+    return err; 
+  };
+
+  double CalculateOverlapReIm(LISAParams params1, LISAParams params2, LISAInjectionReIm * injection)
+  {
+    double overlap = -DBL_MAX;
+    int ret;
+    
+    /* Frequency vector - assumes common to A,E,T, i.e. identical fLow, fHigh in all channels */
+    gsl_vector* freq = injection->freq;
+    
+    /* Generating the signal in the three detectors for the input parameters */
+    LISASignalReIm* signal1 = NULL;
+    LISASignalReIm* signal2 = NULL;
+    LISASignalReIm_Init(&signal1);
+    LISASignalReIm_Init(&signal2);
+    ret = LISAGenerateSignalReIm(&params1, freq, signal1);
+    if(ret==SUCCESS){
+      ret = LISAGenerateSignalReIm(&params2, freq, signal2);
+    }
+    /* If LISAGenerateSignal failed (e.g. parameters out of bound), silently return -Infinity logL */
+    if(ret==FAILURE) {
+      overlap = -DBL_MAX;
+    }
+    else if(ret==SUCCESS) {
+      /* Computing the likelihood for each TDI channel - fstartobs has already been taken into account */
+      double loglikelihoodTDI1 = FDLogLikelihoodReIm(signal1->TDI1Signal, signal2->TDI1Signal, injection->noisevalues1);
+      double loglikelihoodTDI2 = FDLogLikelihoodReIm(signal1->TDI2Signal, signal2->TDI2Signal, injection->noisevalues2);
+      double loglikelihoodTDI3 = FDLogLikelihoodReIm(signal1->TDI3Signal, signal2->TDI3Signal, injection->noisevalues3);
+      overlap = loglikelihoodTDI1 + loglikelihoodTDI2 + loglikelihoodTDI3;
+    }
+    
+    /* Clean up */
+    LISASignalReIm_Cleanup(signal1);
+    LISASignalReIm_Cleanup(signal2);
+
+    //cout<<" overlap="<<overlap<<endl;
+    return overlap;
+  }
 
 };
-
 
 //***************************************************************************************8
 //main test program
 int main(int argc, char*argv[]){
+  int myid = 0;
+
+  //This tell LISAinterface_common not to call MPI stuff;
+  noMPI=1;
+
   //string datafile;
   const int NparRead=Npar; 
 
@@ -141,19 +270,13 @@ int main(int argc, char*argv[]){
 
   s0->addOptions(opt,"");
   opt.add(Option("nchains","Number of consequtive chain runs. Default 1","1"));
-  opt.add(Option("info_every","How often to dump likehood eval info to stdout. Default never","0"));
+  opt.add(Option("info_every","How often to dump likehood eval info to stdout. Default never","10000"));
   opt.add(Option("rng_seed","Pseudo random number grenerator seed in [0,1). (Default=-1, use clock to seed.)","-1"));
   opt.add(Option("precision","Set output precision digits. (Default 13).","13"));
+  opt.add(Option("Fisher_err_target","Set target for Fisher error measure. (Default 0.001).","0.001"));
   opt.add(Option("help","Print help message."));
   //First we parse the ptmcmc-related parameters like un gleam. 
   opt.parse(argc,argv,false);
-  if(opt.set("help")){
-    cout<<"****** PTMCMC options *******\n"<<opt.print_usage()<<endl;
-    static char help[]="--help";
-    argv[argc]=help;
-    argc++;
-  } else cout<<"ptmcmc flags=\n"<<opt.report()<<endl;
-
   
   //Next we perform the initializations from LISAinference.c
   LISARunParams runParams={};
@@ -161,6 +284,17 @@ int main(int argc, char*argv[]){
   int *freeparamsmap = NULL;
   void *context = NULL;
   double logZtrue=0;
+  if(opt.set("help")){
+    cout<<"****** PTMCMC options *******\n"<<opt.print_usage()<<endl;
+    //The rest of this is just to make the code generate the usage message.
+    LISAParams arg3;
+    LISAGlobalParams arg4;
+    LISAPrior arg5;
+    LISARunParams arg6;
+    char* tmpargv[2]={argv[0],(char*)"--help"};
+    parse_args_LISA(2, tmpargv, &arg3, &arg4, &arg5, &arg6);
+  } else cout<<"ptmcmc flags=\n"<<opt.report()<<endl;
+
   addendum(argc,argv,&runParams,&ndim,&nPar,&freeparamsmap,&context,&logZtrue);
 
   //Post parse setup
@@ -171,6 +305,7 @@ int main(int argc, char*argv[]){
   istringstream(opt.value("nchains"))>>Nchain;
   istringstream(opt.value("rng_seed"))>>seed;
   istringstream(opt.value("info_every"))>>info_every;
+  istringstream(opt.value("Fisher_err_target"))>>fisher_err_target;
   
   //if seed<0 set seed from clock
   if(seed<0)seed=fmod(time(NULL)/3.0e7,1);
@@ -181,6 +316,9 @@ int main(int argc, char*argv[]){
   //output location
   string outname(runParams.outroot);
   ostringstream ss("");
+  ss<<outname;
+  string base=ss.str();
+  
 
   //report
   cout.precision(output_precision);
@@ -278,6 +416,89 @@ int main(int argc, char*argv[]){
   like->reset();
   if(info_every>0)fl.info_every(info_every);
   
+  //Next compute the Fisher matrix at the injected state.
+  ss.str("");ss<<base<<"_fishcov.dat";
+  ofstream outfish(ss.str().c_str());
+  vector< vector<double> > fim(Npar,vector<double>(Npar));
+  double err=like->getFisher(injected_state,fim);
+  cout<<"Fisher err ~"<<err<<endl;
+  cout<<"At pars:\n"<<endl;
+  for(int i=0;i<Npar;i++)cout<<names[i]<<"\t";
+  cout<<endl;
+  cout<<"\nFisher at injection:"<<endl;
+  outfish<<"#At pars:\n#";
+  for(int i=0;i<Npar;i++)outfish<<names[i]<<"\t";
+  outfish<<endl;
+  for(int i=0;i<Npar;i++)outfish<<injected_state.get_param(i)<<"\t";
+  outfish<<endl;
+  outfish<<"\n#Fisher:"<<endl;
+  for(int i=0;i<Npar;i++){
+    cout<<names[i]<<"\t";
+    for(int j=0;j<=i;j++)cout<<"\t"<<fim[i][j];
+    cout<<endl;
+    for(int j=0;j<=i;j++)outfish<<"\t"<<fim[i][j];
+    for(int j=i+1;j<Npar;j++)outfish<<"\t"<<fim[i][j];
+    outfish<<endl;
+  }
+  //Next dump Fisher scaled by diagonals;
+  cout<<"Fisher scales:"<<endl;
+  for(int i=0;i<Npar;i++)cout<<sqrt(fim[i][i])<<"\t";
+  cout<<endl;
+  outfish<<"\n#Fisher scales:"<<endl;
+  for(int i=0;i<Npar;i++)outfish<<sqrt(fim[i][i])<<"\t";
+  outfish<<endl;
+  cout<<"\nScaled Fisher:"<<endl;
+  outfish<<"\n#Scaled Fisher:"<<endl;
+  for(int i=0;i<Npar;i++){
+    cout<<names[i]<<"\t";
+    for(int j=0;j<=i;j++)cout<<"\t"<<fim[i][j]/sqrt(fim[i][i]*fim[j][j]);
+    cout<<endl;
+    for(int j=0;j<=i;j++)outfish<<"\t"<<fim[i][j]/sqrt(fim[i][i]*fim[j][j]);
+    for(int j=i+1;j<Npar;j++)outfish<<"\t"<<fim[i][j]/sqrt(fim[i][i]*fim[j][j]);
+    outfish<<endl;
+  }
+  //Now invert it to get the covariance matrix
+  //We use GSLs Cholesky decomposition for this.
+  gsl_set_error_handler_off();
+  gsl_matrix * fishcov=gsl_matrix_alloc(Npar,Npar);
+  for(int i=0;i<Npar;i++)for(int j=0;j<=i;j++)gsl_matrix_set(fishcov,i,j,fim[i][j]);//Don't need upper triangle for GSLs routine
+  if(gsl_linalg_cholesky_decomp(fishcov))cout<<"Fisher matrix Cholesky decomposition failed."<<endl;
+  else if(gsl_linalg_cholesky_invert(fishcov))cout<<"Fisher matrix Cholesky inverse failed."<<endl;
+  else {
+    cout<<"\nCovariance:"<<endl;
+    outfish<<"\n#Covariance:"<<endl;
+    for(int i=0;i<Npar;i++){
+      cout<<names[i]<<"\t";
+      for(int j=0;j<=i;j++)cout<<"\t"<<gsl_matrix_get(fishcov,i,j);
+      cout<<endl;
+      for(int j=0;j<=i;j++)outfish<<"\t"<<gsl_matrix_get(fishcov,i,j);
+      for(int j=i+1;j<Npar;j++)outfish<<"\t"<<gsl_matrix_get(fishcov,i,j);
+      outfish<<endl;
+    }
+    //Now the correlation matrix:
+    cout<<"\nCovar scales:"<<endl;
+    for(int i=0;i<Npar;i++)cout<<sqrt(gsl_matrix_get(fishcov,i,i))<<"\t";
+    outfish<<"\n#Covar scales:"<<endl;
+    for(int i=0;i<Npar;i++)outfish<<sqrt(gsl_matrix_get(fishcov,i,i))<<"\t";
+    cout<<"\n\nCorrelation:"<<endl;
+    outfish<<"\n\n#Coorrelation:"<<endl;
+    for(int i=0;i<Npar;i++){
+      cout<<names[i]<<"\t";
+      for(int j=0;j<=i;j++)cout<<"\t"<<gsl_matrix_get(fishcov,i,j)/sqrt(gsl_matrix_get(fishcov,i,i)*gsl_matrix_get(fishcov,j,j));
+      cout<<endl;
+      for(int j=0;j<=i;j++)outfish<<"\t"<<gsl_matrix_get(fishcov,i,j)/sqrt(gsl_matrix_get(fishcov,i,i)*gsl_matrix_get(fishcov,j,j));
+  for(int j=i+1;j<Npar;j++)outfish<<"\t"<<gsl_matrix_get(fishcov,i,j)/sqrt(gsl_matrix_get(fishcov,i,i)*gsl_matrix_get(fishcov,j,j));
+      outfish<<endl;
+    }
+  }
+  //Close-out Fisher/Covar
+  gsl_matrix_free(fishcov);
+  outfish.close();
+  if(stoi(opt.value("nsteps"))<=0){
+    cout<<"No MCMC steps requested"<<endl;
+    exit(0);
+  }
+
   //assuming mcmc:
   //Set the proposal distribution 
   int Ninit;
@@ -286,12 +507,6 @@ int main(int argc, char*argv[]){
   //set up the mcmc sampler (assuming mcmc)
   mcmc.setup(Ninit,*like,*prior,*prop,output_precision);
 
-
-  //Prepare for chain output
-  //ss<<"gle_"<<outname;
-  ss<<outname;
-  string base=ss.str();
-  
   //Loop over Nchains
   for(int ic=0;ic<Nchain;ic++){
     bayes_sampler *s=s0->clone();
