c39dcc59e0baf9238298b423d57e14c09c25882c
John Baker <john.g.baker@nasa.gov>
2017-05-22 13:27:35 -0400

    Updated to new flare.py.  Not yet formaized..

--- python/FisherStudy.py
+++ python/FisherStudy.py
@@ -6,6 +6,8 @@ import numpy as np
 flare.flare_dir="../flare"
 #flare.LISAvariant="LISA2017";label="LISA2017c/"
 flare.deltatobs=10.0;flare.LISAvariant="LISA2017";label="LISA2017c_10yr/"
+flare.FisherReIm=False;flare.deltatobs=10.0;flare.LISAvariant="LISA2017";label="LISA2017camp_10yr/"
+#flare.FisherReIm=True;flare.deltatobs=10.0;flare.LISAvariant="LISA2017";label="LISA2017reim_10yr/"
 #label="2arm-LISA/";flare.extra_flags=" --tagtdi TDIAXYZ"
 #label="tRef-redef-LISA2017/"
 #label="fast-orbit-LISA/"
@@ -19,7 +21,7 @@ Ms=[150,470,1.5e3,4.7e3,1.5e4,1.5e5,1.5e5*10**0.5]+Ms
 #Ms=[4.7e3,1.5e4,1.5e5,1.5e5*10**0.5]+Ms
 #Ms=[1.5e3,4.7e3,1.5e4,1.5e5,1.5e5*10**0.5]+Ms
 print "Ms=",Ms
-flare.SNRstudy(label,Ms,[2.0],[10,100,1000],300)
+#flare.SNRstudy(label,Ms,[2.0],[10,100,1000],300)
 #/////#/////flare.SNRstudy(label,Ms,[2.0],[10,100,1000],30,8)
 #flare.SNRstudy(label,Ms,[2.0],[100],30,8)
 ##flare.SNRstudy(Ms,[1,2,4,10],[10,100,1000],300)
